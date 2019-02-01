3404c5a553db2795d8e525986254d3a59799637c
ykisialiou <hiluro@gmail.com>
2019-02-01 15:47:17 +0300

    Uppercase recieved data

--- process-joke.js
+++ process-joke.js
@@ -4,6 +4,7 @@
 module.exports = x => {
 	console.log('Process payload')
 	var out = JSON.stringify(x);
-	console.log(`Recieved ${out}`)
-	return x
+	var uppercase = out.toUpperCase(); 
+	console.log(`Recieved ${uppercase}`)
+	return uppercase 
 }
